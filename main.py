9058b35eeeb0ec8d79ef1f043f194cab7890b050
DarioKillenberger <s3787455@student.rmit.edu.au>
2022-06-20 15:14:10 +1000

    t

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import streamlit as st
 st.title('Careers Paths Story Game')
 st.write("""You watch your team through the cameras. Where do you tell them to enter the museum""")
 
+st.button("Through the roof", key=None, help=None, on_click=st.write("""test""")
+)
 st.radio("Choose one", ["Through the roof", "Through the main door", "Through the staff door"])
+
+
